7bd86ca9e964d74f89fa8d7513a7800f17820289
LouisSchmieder <dev@louis-schmieder.de>
2020-06-29 00:46:27 +0200

    Fixed find_comments

--- comment.v
+++ comment.v
@@ -13,11 +13,11 @@ mut:
 }
 
 fn (mut app App) find_issue_comments(issue_id int) []Comment {
-	mut comments = sql app.db {
+	mut comments := sql app.db {
 		select from Comment where issue_id == issue_id 
 	}
 	for i, comment in comments {
 		comments[i].author_name = app.find_username_by_id(comment.author_id)
 	}
-	return commands
+	return comments
 }
