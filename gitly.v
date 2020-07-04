e735f9dd5a5b6894886118d337df0f351bd3b5f1
JalonSolov <JalonSolov@gmail.com>
2020-07-04 12:55:08 -0400

    made some error messages more verbose

--- gitly.v
+++ gitly.v
@@ -104,7 +104,7 @@ pub fn (mut app App) init_once() {
 
 	if !os.exists(repo_storage_path) {
 		os.mkdir(repo_storage_path) or {
-			app.error('Repo storage can not created')
+			app.error('Problem encountered while attempting to create Repo storage')
 			app.error('Error: $err')
 			exit(1)
 		}
