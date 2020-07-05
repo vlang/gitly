9abbd73988b6e138b6710c7447b9d0f73c6d3135
LouisSchmieder <dev@louis-schmieder.de>
2020-07-06 00:33:13 +0200

    add link parsing for oauth

--- oauth.v
+++ oauth.v
@@ -5,8 +5,9 @@ import vweb
 import json
 import net.http
 
-pub fn (mut app App) oauth() vweb.Result {
-	code := app.vweb.req.url.all_after('code=')
+pub fn (mut app App) oauth(parameter string) vweb.Result {
+	println(parameter)
+	code := parameter.all_after('code=')
 	if code == '' {
 		return app.vweb.not_found()
 	}
