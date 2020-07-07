956092c6871e94cf595ea9abef6cb45a2bdb8a8c
Louis Schmieder <dev@louis-schmieder.de>
2020-07-07 14:23:30 +0200

    add max user repos (#63)

--- repo_db.v
+++ repo_db.v
@@ -174,6 +174,12 @@ fn (app &App) find_repo_by_name(user int, name string) ?Repo {
 	return x[0]
 }
 
+fn (app &App) nr_user_repos(user_id int) int {
+	return sql app.db {
+		select count from Repo where user_id == user_id
+	}
+}
+
 fn (app &App) find_user_repos(user_id int) []Repo {
 	return sql app.db {
 		select from Repo where user_id == user_id
