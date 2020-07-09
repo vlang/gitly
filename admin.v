84c2b902e507f3909c00461bdca4faa8e9eb62fe
LouisSchmieder <dev@louis-schmieder.de>
2020-07-09 16:52:47 +0200

    add update function

--- admin.v
+++ admin.v
@@ -1,6 +1,7 @@
 module main
 
 import vweb
+import os
 
 pub fn (mut app App) admin() vweb.Result {
 	if !app.is_admin() {
@@ -53,6 +54,43 @@ pub fn (mut app App) admin_edituser_post() vweb.Result {
 	}
 
 	return app.vweb.text('TODO')
+}
+
+['/admin/update']
+pub fn (mut app App) admin_update() vweb.Result {
+	if !app.is_admin() {
+		return app.vweb.redirect('/')
+	}
+	app.info('Update V')
+	mut res := os.exec('v up') or { os.Result{exit_code: 1} }
+
+	if res.exit_code != 0 {
+		app.info('Failed to update V')
+		app.info(res.output)
+		return app.vweb.redirect('/')
+	}
+
+	res = os.exec('git diff') or { os.Result{exit_code: 1} }
+
+	if res.exit_code != 0 || res.output != '' {
+		app.info('Failed to update Gitly')
+		return app.vweb.redirect('/')
+	}
+
+	res = os.exec('git checkout master') or { os.Result{exit_code: 1} }
+	if res.exit_code != 0 {
+		app.info('Failed to checkout master for Gitly')
+		return app.vweb.redirect('/')
+	}
+
+	res = os.exec('git pull') or { os.Result{exit_code: 1} }
+	if res.exit_code != 0 {
+		app.info('Failed to update Gitly')
+		app.info(res.output)
+		return app.vweb.redirect('/')
+	}
+	app.info('Updated gitly successful')
+	return app.vweb.redirect('/')
 }
 
 fn (mut app App) is_admin() bool {
