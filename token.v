56d49c8d7ff67fe14ba66d633f1cfc83c7600719
Louis Schmieder <dev@louis-schmieder.de>
2020-07-14 13:33:52 +0200

    use old tokens

--- token.v
+++ token.v
@@ -9,25 +9,16 @@ struct Token {
 	ip string
 }
 
-fn (mut app App) update_user_token(user_id int, token, ip string) {
+fn (mut app App) update_user_token(user_id int, token, ip string) string {
 	tok := app.find_user_token(user_id, ip)
 	if tok == '' {
 		new_token := Token{user_id: user_id, value: token, ip: ip }
 		sql app.db {
 			insert new_token into Token
 		}
+		return token
 	}
-	else {
-    sql app.db {
-			// TODO fix a bug in ORM
-			//update Token set value = token where user_id == user_id
-			delete from Token where user_id == user_id && ip == ip
-		}
-    new_token := Token{user_id: user_id, value: token, ip: ip }
-		sql app.db {
-			insert new_token into Token
-		}
-	}
+	return tok
 }
 
 fn (mut app App) find_user_token(user_id int, ip string) string {
@@ -44,8 +35,8 @@ fn (mut app App) clear_sessions(user_id int) {
 }
 
 fn (mut app App) add_token(user_id int, ip string) string {
-	token := gen_uuid_v4ish()
-	app.update_user_token(user_id, token, ip)
+	mut token := gen_uuid_v4ish()
+	token = app.update_user_token(user_id, token, ip)
 	return token
 }
 
