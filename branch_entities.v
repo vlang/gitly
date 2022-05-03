b5403ebddfb0da66c1d83cfb531053bcf08a2274
walkingdevel <mosquito.is.dead@gmail.com>
2022-05-03 16:13:09 +0000

    refactor branches

--- branch_entities.v
+++ branch_entities.v
@@ -6,7 +6,7 @@ struct Branch {
 mut:
 	id      int    [primary; sql: serial]
 	repo_id int    [unique: 'branch']
-	name    string [unique: 'branch'] // branch name
+	name    string [unique: 'branch']
 	author  string // author of latest commit on branch
 	hash    string // hash of latest commit on branch
 	date    int    // time of latest commit on branch
