17c4f11d1b7d369f9e88598e71459fa8ca626d11
walkingdevel <mosquito.is.dead@gmail.com>
2022-05-04 15:58:39 +0000

    refactor: files

--- file_entities.v
+++ file_entities.v
@@ -3,16 +3,16 @@
 module main
 
 struct File {
-	id              int    [primary; sql: serial]
-	repo_id         int    [unique: 'file']
-	name            string [unique: 'file']
-	parent_path     string [unique: 'file']
-	is_dir          bool
-	branch          string [unique: 'file']
-	nr_contributors int
-	last_hash       string
-	size            int
-	nr_views        int
+	id                 int    [primary; sql: serial]
+	repo_id            int    [unique: 'file']
+	name               string [unique: 'file']
+	parent_path        string [unique: 'file']
+	is_dir             bool
+	branch             string [unique: 'file']
+	contributors_count int
+	last_hash          string
+	size               int
+	views_count        int
 mut:
 	last_msg  string
 	last_time int
