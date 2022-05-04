46a828ef776c325cb875a8326bf9d7d89fee5fca
walkingdevel <mosquito.is.dead@gmail.com>
2022-05-04 16:05:14 +0000

    refactor: releases

--- release_routes.v
+++ release_routes.v
@@ -8,15 +8,20 @@ pub fn (mut app App) releases(user_str string, repo string) vweb.Result {
 	if !app.exists_user_repo(user_str, repo) {
 		return app.not_found()
 	}
+
 	app.show_menu = true
+
 	mut releases := []Release{}
 	mut release := Release{}
+
 	tags := app.get_all_repo_tags(app.repo.id)
 	rels := app.find_repo_releases(app.repo.id)
 	users := app.find_repo_registered_contributor(app.repo.id)
+
 	for rel in rels {
 		release.notes = rel.notes
 		mut user_id := 0
+
 		for tag in tags {
 			if tag.id == rel.tag_id {
 				release.tag_name = tag.name
@@ -34,5 +39,6 @@ pub fn (mut app App) releases(user_str string, repo string) vweb.Result {
 		}
 		releases << release
 	}
+
 	return $vweb.html()
 }
