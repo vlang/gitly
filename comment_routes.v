d6d75a00706345c95bcfbc509df7c262eca717d1
walkingdevel <mosquito.is.dead@gmail.com>
2022-05-10 14:28:13 +0000

    fix: issue validation, show errors

--- comment_routes.v
+++ comment_routes.v
@@ -1,6 +1,7 @@
 module main
 
 import vweb
+import validation
 
 ['/:user/:repo/comments'; post]
 pub fn (mut app App) handle_add_comment(user string, repo string) vweb.Result {
@@ -11,8 +12,13 @@ pub fn (mut app App) handle_add_comment(user string, repo string) vweb.Result {
 	text := app.form['text']
 	issue_id := app.form['issue_id']
 
-	if text == '' || issue_id == '' || !app.logged_in {
-		return app.redirect('/$user/$repo/issue/$issue_id')
+	is_text_empty := validation.is_string_empty(text)
+	is_issue_id_empty := validation.is_string_empty(issue_id)
+
+	if is_text_empty || is_issue_id_empty || !app.logged_in {
+		app.error('Issue comment is not valid')
+
+		return app.issue(user, repo, issue_id)
 	}
 
 	app.add_issue_comment(app.user.id, issue_id.int(), text)
