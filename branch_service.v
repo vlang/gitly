c7f97d02be8a2945926831e8b0237b67c858db10
walkingdevel <mosquito.is.dead@gmail.com>
2022-05-12 17:33:07 +0000

    fix: fetch primary branch not only master

--- branch_service.v
+++ branch_service.v
@@ -4,6 +4,7 @@ import os
 import time
 
 fn (mut app App) fetch_branches(r Repo) {
+	primary_branch := r.primary_branch
 	all_branches := r.git('branch -a')
 
 	for remote_branch in all_branches.split_into_lines() {
@@ -42,10 +43,11 @@ fn (mut app App) fetch_branches(r Repo) {
 		}
 	}
 
-	r.git('checkout master')
+	r.git('checkout $primary_branch')
 }
 
 fn (mut app App) update_branches(r &Repo) {
+	primary_branch := r.primary_branch
 	all_branches := r.git('branch -a')
 
 	for remote_branch in all_branches.split_into_lines() {
@@ -90,7 +92,7 @@ fn (mut app App) update_branches(r &Repo) {
 		}
 	}
 
-	r.git('checkout master')
+	r.git('checkout $primary_branch')
 }
 
 fn (mut app App) create_branch(repo_id int, name string, author string, hash string, date int) {
