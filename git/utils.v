6f442545b8a982c392c63c7d6d7f2673546e1216
walkingdevel <mosquito.is.dead@gmail.com>
2022-05-19 16:29:13 +0000

    feature: get git path

--- git/utils.v
+++ git/utils.v
@@ -3,6 +3,18 @@ module git
 import regex
 import os
 
+pub fn get_git_executable_path() ?string {
+	which_result := os.execute('which git')
+	which_exit_code := which_result.exit_code
+	which_output := which_result.output
+
+	if which_exit_code != 0 {
+		return none
+	}
+
+	return which_output.trim(' \n')
+}
+
 pub fn get_repository_primary_branch(path string) string {
 	git_result := os.execute('git -C $path symbolic-ref HEAD')
 	git_exit_code := git_result.exit_code
