ed4142eb5770828ab7657cd7ead03d1b2afaa278
walkingdevel <mosquito.is.dead@gmail.com>
2022-05-26 16:25:29 +0000

    refactor: use fields method

--- git/branch.v
+++ git/branch.v
@@ -3,8 +3,7 @@ module git
 // parse_git_branch_with_last_hash parses output from `git branch -av`
 // returns the branch name with hash of the last commit
 pub fn parse_git_branch_output(output string) (string, string) {
-	output_parts := output.split(' ')
-
+	output_parts := output.fields()
 	asterisk_or_branch_name := output_parts[0]
 
 	if asterisk_or_branch_name == '*' {
