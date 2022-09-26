14e86055c0573c3cc4ff2ebcc86f1334b397bdcb
walkingdevel <mosquito.is.dead@gmail.com>
2022-09-26 20:42:45 +0000

    fix: remove comments from markdown files.

--- highlight/markdown.v
+++ highlight/markdown.v
@@ -1,6 +1,7 @@
 module highlight
 
 import markdown
+import regex
 
 pub fn convert_markdown_to_html(code string) string {
 	markdown_code := sanitize_markdown_code(code)
@@ -11,6 +12,9 @@ pub fn convert_markdown_to_html(code string) string {
 // temporary solution while markdown module doesn't support sanitizing and escaping HTML tags
 pub fn sanitize_markdown_code(code string) string {
 	// FIXME: > in `sassc static/css/gitly.scss > static/css/gitly.css`
-	// FIXME: remove comments <!-- -->
-	return code.replace_each(['<', '&lt;', '>', '&gt;'])
+	remove_comments_query := r'<!--[\S\s]*?-->'
+	mut remove_comments_re := regex.regex_opt(remove_comments_query) or { return code }
+	code_no_comments := remove_comments_re.replace(code, '')
+
+	return code_no_comments.replace_each(['<', '&lt;', '>', '&gt;'])
 }
