8ae91d141d2c218c3c0121606bd263fa4551fefe
Swastik Baranwal <swstkbaranwal@gmail.com>
2022-11-05 15:58:03 +0530

    crypto: update const type (#221)

--- src/crypto.v
+++ src/crypto.v
@@ -4,7 +4,7 @@ import rand
 import crypto.rand as crypto_rand
 import encoding.base64
 
-pub const max_safe_unsigned_integer = 4_294_967_295
+pub const max_safe_unsigned_integer = u32(4_294_967_295)
 
 pub fn set_rand_crypto_safe_seed() {
 	first_seed := generate_crypto_safe_int_u32()
