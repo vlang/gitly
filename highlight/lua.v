d0e1f3ad2fa3d76306a3de11642f5ff50e9e9ede
Francesco La Camera <fm@lacamera.org>
2023-04-13 15:25:47 +0200

    highlight: add support for lua (#242)

---
+++ highlight/lua.v
@@ -0,0 +1,36 @@
+module highlight
+
+fn init_lua() Lang {
+	return Lang{
+		name: 'Lua'
+    lang_extensions: ['lua']
+		line_comments: '--'
+    mline_comments: ['--[[', ']]']
+		string_start: ['"', "'"]
+		color: '#00007d'
+		keywords: [
+      'and',
+      'break',
+      'do',
+      'else',
+      'elseif',
+      'end',
+      'false',
+      'for',
+      'function',
+      'goto',
+      'if',
+      'in',
+      'local',
+      'nil',
+      'not',
+      'or',
+      'repeat',
+      'return',
+      'then',
+      'true',
+      'until',
+      'while',
+		]
+	}
+}
