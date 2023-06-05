b411f1be89f106799faca0c4f1b440d0691c45f3
walkingdevel <mosquito.is.dead@gmail.com>
2023-06-05 18:23:12 +0000

    fix: byte -> u8

--- highlight/lua.v
+++ highlight/lua.v
@@ -3,34 +3,34 @@ module highlight
 fn init_lua() Lang {
 	return Lang{
 		name: 'Lua'
-    lang_extensions: ['lua']
+		lang_extensions: ['lua']
 		line_comments: '--'
-    mline_comments: ['--[[', ']]']
+		mline_comments: ['--[[', ']]']
 		string_start: ['"', "'"]
 		color: '#00007d'
 		keywords: [
-      'and',
-      'break',
-      'do',
-      'else',
-      'elseif',
-      'end',
-      'false',
-      'for',
-      'function',
-      'goto',
-      'if',
-      'in',
-      'local',
-      'nil',
-      'not',
-      'or',
-      'repeat',
-      'return',
-      'then',
-      'true',
-      'until',
-      'while',
+			'and',
+			'break',
+			'do',
+			'else',
+			'elseif',
+			'end',
+			'false',
+			'for',
+			'function',
+			'goto',
+			'if',
+			'in',
+			'local',
+			'nil',
+			'not',
+			'or',
+			'repeat',
+			'return',
+			'then',
+			'true',
+			'until',
+			'while',
 		]
 	}
 }
