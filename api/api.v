53179f2076d9c675b92e8041b5ecf802d76131bd
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-04-09 13:15:45 +0200

    add `pub` access modifier to publicly used struct fields (#263)

--- api/api.v
+++ api/api.v
@@ -1,16 +1,19 @@
 module api
 
 pub struct ApiErrorResponse {
+pub:
 	success bool
 	message string
 }
 
 pub struct ApiResponse {
+pub:
 	success bool
 	message string
 }
 
 pub struct ApiSuccessResponse[T] {
+pub:
 	success bool
 	result  T
 }
