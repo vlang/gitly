57faba47a3bf6cd30c11a31221870e6ba41bcc4e
Alexander Medvednikov <alexander@medvednikov.com>
2026-03-11 08:53:48 +0300

    make compile with latest V

--- src/utils.v
+++ src/utils.v
@@ -10,9 +10,9 @@ pub fn (mut app App) running_since() string {
 
 	seconds_in_hour := 60 * 60
 
-	days := int(math.floor(duration / (seconds_in_hour * 24)))
-	hours := int(math.floor(duration / seconds_in_hour % 24))
-	minutes := int(math.floor(duration / 60)) % 60
+	days := int(math.floor(f64(duration) / f64(seconds_in_hour * 24)))
+	hours := int(math.fmod(math.floor(f64(duration) / f64(seconds_in_hour)), 24))
+	minutes := int(math.floor(f64(duration) / 60.0)) % 60
 	seconds := duration % 60
 
 	return '${days} days ${hours} hours ${minutes} minutes and ${seconds} seconds'
